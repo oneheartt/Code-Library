cedc69759f99f4a1f33441ed3b87e51f02a36bfa
oneheartt <100949404+oneheartt@users.noreply.github.com>
2025-09-27 19:03:08 +0700

    Update Mo's Algorithm.cpp

--- Data Structure/Mo's Algorithm.cpp
+++ Data Structure/Mo's Algorithm.cpp
@@ -10,12 +10,12 @@ struct q_data{
     }
 }query[maxN];
 
-    int L = 1, R = 0;
-    rep(i,1,q){
-        int l = query[i].l, r = query[i].r, id = query[i].id;
-        while(L < l) del(L++);
-        while(L > l) add(--L);
-        while(R < r) add(++R);
-        while(R > r) del(R--);
-        ans[id] = ok;
-    }
+int L = 1, R = 0;
+rep(i,1,q){
+    int l = query[i].l, r = query[i].r, id = query[i].id;
+    while(L < l) del(L++);
+    while(L > l) add(--L);
+    while(R < r) add(++R);
+    while(R > r) del(R--);
+    ans[id] = ok;
+}
